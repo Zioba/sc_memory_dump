c8ae03011e0217fcb672ebea397bbcc394faad2e
Your Name <you@example.com>
2020-02-12 15:22:40 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/wave.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/artsiom/Desktop/wave_find_path_sc_memory-master/utils.cpp" "/home/artsiom/Desktop/wave_find_path_sc_memory-master/cmake-build-debug/CMakeFiles/wave.dir/utils.cpp.o"
+  "/home/artsiom/Desktop/wave_find_path_sc_memory-master/wavefindpath.cpp" "/home/artsiom/Desktop/wave_find_path_sc_memory-master/cmake-build-debug/CMakeFiles/wave.dir/wavefindpath.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/artsiom/work/ostis/sc-machine/sc-memory"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ wavefindpath.cpp
@@ -0,0 +1,62 @@
+extern "C" {
+#include "sc_memory_headers.h"
+#include "sc_helper.h"
+#include "utils.h"
+}
+
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+sc_memory_context *context;
+
+sc_addr graph, rrel_arcs, rrel_nodes;
+FILE *f;
+
+void print_graph()
+{
+    printEl(context, graph, f);
+    fprintf(f, "\n----------------------\n");
+
+    sc_iterator3 *it = sc_iterator3_f_a_a_new(context,
+                       graph,
+                       sc_type_arc_pos_const_perm,
+                       0);
+
+    while (SC_TRUE == sc_iterator3_next(it)) {
+        sc_addr t_arc = sc_iterator3_value(it, 2);
+        printEl(context, t_arc, f);
+        fprintf(f, "\n");
+    }
+    sc_iterator3_free(it);
+}
+
+void run_test()
+{;
+
+    char gr[12] = "rootElement";
+    //char gr[] = "graph";
+    sc_helper_resolve_system_identifier(context, gr, &graph);
+    print_graph();
+}
+
+int main()
+{
+    sc_memory_params params;
+    sc_memory_params_clear(&params);
+    params.repo_path = "/home/artsiom/work/ostis/kb.bin";
+    params.config_file = "/home/artsiom/work/ostis/config/sc-web.ini";
+    params.ext_path = "/home/artsiom/work/ostis/sc-machine/bin/extensions";
+    params.clear = SC_FALSE;
+    sc_memory_initialize(&params);
+    context = sc_memory_context_new(sc_access_lvl_make_max);
+    f = fopen("/home/artsiom/Desktop/KnowledgeDump.txt", "w");
+    run_test();
+    fclose(f);
+    sc_memory_context_free(context);
+    sc_memory_shutdown(SC_TRUE);
+
+    return 0;
+}
